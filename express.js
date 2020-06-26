8aa2041b249ed7d443ae7bbe8cb4421a9b19a12f
Jason Pallone <jayboii478@gmail.com>
2020-06-25 21:44:09 -0400

    Heroku test

--- express.js
+++ express.js
@@ -43,6 +43,15 @@ app.use((err, req, res, next) => {
   });
 });
 
+/* for heroku deployment */
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Set our port.
 app.set('port', process.env.PORT || 5000);
 
